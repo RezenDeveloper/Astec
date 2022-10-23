a801250b275a4d11150e6d851e13d8c20ccab4b6
Rezende <rezendeveloper@gmail.com>
2022-10-23 17:33:12 -0300

    created pdf routes

---
+++ src/pages/api/pdf/index.ts
@@ -0,0 +1,113 @@
+import { google } from 'googleapis'
+import settings from '../../../../google-settings.json'
+import fs from 'fs'
+import path from 'path'
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case 'GET': return await getPDF(req, res)
+    case 'POST': return await createPDF(req, res)
+    case 'DELETE': return await deletePDF(req, res)
+    default: return res.status(500).send(`Invalid method`)
+  }
+}
+
+const getDrive = () => {
+  const j2 = new google.auth.JWT(
+    settings.client_email, 
+    undefined, 
+    settings.private_key,
+    'https://www.googleapis.com/auth/drive', 
+    undefined
+  )
+  
+  return google.drive({
+    version: "v3",
+    auth: j2
+  })
+}
+
+const getPDF = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    
+    const drive = getDrive()
+    const id = req.query.id
+
+    if(!id) {
+      const { data:pdf } = await drive.files.list({
+        fields: 'files/webViewLink, files/webContentLink, files/name, files/id'
+      })
+
+      return res.status(200).send(pdf)
+    }
+
+    const { data:pdf } = await drive.files.get({
+      fileId: id as string,
+      fields: '*'
+    })
+
+    res.send(pdf)
+  } catch (error) {
+    console.log(error)
+    res.status(400).send('')
+  }
+}
+
+const createPDF = async (req: NextApiRequest, res: NextApiResponse) => {  
+  try {
+    const drive = getDrive()
+    const pdfPath = path.resolve(__dirname, '../../../../public/test.pdf')
+    const file = fs.createReadStream(pdfPath)
+
+    const { data:createData } = await drive.files.create({
+      requestBody: {
+        name: 'teste'
+      },
+      media: {
+        mimeType: 'application/pdf',
+        body: file
+      }
+    })
+
+    const fileId = createData.id
+    if(!fileId) throw new Error('File not found')
+
+    await drive.permissions.create({
+      fileId,
+      requestBody: {
+        role: 'reader',
+        type: 'anyone'
+      }
+    })
+
+    const { data } = await drive.files.get({
+      fileId,
+      fields: 'webViewLink, webContentLink'
+    })
+
+    console.log(data)
+  
+    res.status(200).send(data)    
+  } catch (error) {
+    console.log(error)
+    res.status(400).send('')
+  }
+}
+
+const deletePDF = async (req: NextApiRequest, res: NextApiResponse) => {
+  const id = req.query.id
+
+  if(!id) res.status(400).send('id not specified')
+
+  const drive = getDrive()
+
+  const { data } = await drive.files.delete({
+    fileId: id as string
+  })
+
+  console.log(data)
+
+  res.status(200).send('Deleted')
+}
