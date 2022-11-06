44e09788d789e04f1781659b74fecaded1283ef4
Rezende <rezendeveloper@gmail.com>
2022-11-06 12:31:44 -0300

    Added create manager route

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/admin/logout')) {
+
+    const response = NextResponse.redirect(new URL('/', request.url))
+    
+    response.cookies.set('TGManager_Admin_Token', '', {
+      expires: new Date()
+    })
+
+    return response
+  }
+}
