30618ba7b1e4b927680259d63bc06b8a74dd4d8d
Rezende <rezendeveloper@gmail.com>
2022-11-06 18:04:52 -0300

    update dialect

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,12 @@
 import { Dialect, Sequelize } from "sequelize";
 import dbConfig from './config/database.js'
+import mysql2 from "mysql2";
 
 const { database, username, password, dialect, ...rest } = dbConfig
 
 const connection = new Sequelize(database!, username!, password!, {
   dialect: dialect as Dialect,
+  dialectModule: mysql2,
   ...rest
 })
 
