502801fc18dcc2ecfa61f724f257b7de5415a462
Rezende <rezendeveloper@gmail.com>
2022-11-12 17:19:40 -0300

    header and footer responsibility

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,21 +6,6 @@ import styles from './styles.module.scss';
 const Footer = () => {
   return (
     <footer className={styles['footer']}>
-      <Image
-        src={'/images/CPS.png'}
-        height={70}
-        width={104}
-      />
-      <Image
-        src={'/images/Fatec.png'}
-        height={57}
-        width={190}
-      />
-      <Image
-        src={'/images/SP.png'}
-        height={78}
-        width={120}
-      />
     </footer>
   );
 }
